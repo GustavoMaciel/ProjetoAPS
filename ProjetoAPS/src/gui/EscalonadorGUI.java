acc6c40dbc67c29a7b0b5331d429bc279d7d23bb
HerculesGit <hercules.silva@dce.ufpb.br>
2019-03-06 14:34:13 -0300

    Criacao da area de configuracoes como tipo de escalonador e valor do quantum

--- ProjetoAPS/src/gui/EscalonadorGUI.java
+++ ProjetoAPS/src/gui/EscalonadorGUI.java
@@ -9,11 +9,11 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Separator;
-import javafx.scene.layout.Border;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -25,20 +25,21 @@ import javafx.stage.Stage;
 
 public class EscalonadorGUI extends Application {
 	private static int TEMPO_EXECUCAO = 30;
+	// private static ArrayList<String> tiposEscalonadores = new
+	// ArrayList<String>();
 
 	private static TabelaResultante tabela;
 	private static VBox areaHistorico;
 	private static BorderPane borderPane;
 	private static ArrayList<Processo> listaProcessos;
-	
+
 	private final static String COLOR_NAO_EXISTE = "#211919";
 	private final static String COLOR_EXECUTANTO = "#77C938";
 	private final static String COLOR_ESPERANDO = "#FFFF19";
 	private final static String COLOR_FINALIZADO = "#EC0C0C";
-	
-	private final static int WIDTH_WINDOWS = 1024; // largura 
-	private final static int HEIGHT_WINDOWS = 500; // altura
-	
+
+	private final static int WIDTH_WINDOWS = 1024; // largura
+	private final static int HEIGHT_WINDOWS = 600; // altura
 
 	/**
 	 * Inicializacao dos componentes
@@ -52,26 +53,26 @@ public class EscalonadorGUI extends Application {
 	 * Cria a regua
 	 */
 	private static VBox criaRegua() {
-		//Rectangle transparent;
+		// Rectangle transparent;
 		Text text;
 		HBox numeros = new HBox();
 		HBox regua = new HBox();
 		// cima, direita, baixo, esquerda
 		regua.setPadding(new Insets(0, 0, 0, 6));
 		numeros.setPadding(new Insets(0, 0, 0, 4));
-		
+
 		Separator s;
 		Text l;
-		
+
 		Label label;
 
 		for (int i = 0; i < TEMPO_EXECUCAO; i++) {
-			label = new Label(""+i);
+			label = new Label("" + i);
 			l = new Text("|");
 			l.setFont(Font.font("Monospaced", 10));
-			text = new Text(50, 150, i+" ");
+			text = new Text(50, 150, i + " ");
 			text.setFont(Font.font("Monospaced", 16));
-			
+
 			if (i < 9) {
 				// cima, direita, baixo, esquerda
 				label.setPadding(new Insets(0, 13, 0, 0));
@@ -79,33 +80,31 @@ public class EscalonadorGUI extends Application {
 				// cima, direita, baixo, esquerda
 				label.setPadding(new Insets(0, 5, 0, 0));
 			}
-			
+
 			label.setMinWidth(10);
 			label.setMaxHeight(10);
 			numeros.getChildren().add(label);
-			
+
 			s = new Separator();
 
 			s.setMinWidth(15);
 			s.setMaxWidth(15);
-			
+
 			regua.getChildren().add(l);
 			regua.getChildren().add(s);
 
 		}
-		
+
 		VBox c = new VBox();
 		c.getChildren().add(numeros);
 		c.getChildren().add(regua);
 		return c;
 	}
 
-	
 	public static void setFilaProcessos(ArrayList<Processo> listaDeProcessos) {
 		listaProcessos = listaDeProcessos;
 	}
-	
-	
+
 	/**
 	 * Cria uma coluna de processos de acordo com os processos que estao na fila
 	 * 
@@ -115,19 +114,17 @@ public class EscalonadorGUI extends Application {
 		VBox v = new VBox();
 		v.setPadding(new Insets(29, 0, 0, 0));
 		HBox container;
-		
+
 		if (listaProcessos == null || listaProcessos.size() == 0)
 			return v;
 
 		System.out.println("entrei pocessos");
 		for (Processo p : listaProcessos) {
-			String str = new StringBuilder().append(p.getProcessoID())
-					.append("(").append(p.getTempoInicio()).append(",")
-					.append(p.getTempoExec()).append(")")
-					.toString();
-			
+			String str = new StringBuilder().append(p.getProcessoID()).append("(").append(p.getTempoInicio())
+					.append(",").append(p.getTempoExec()).append(")").toString();
+
 			container = new HBox();
-			
+
 			// cima, direita, baixo, esquerda
 			container.setPadding(new Insets(1, 0, 0, 0));
 			container.getChildren().add(new Button(str));
@@ -142,7 +139,7 @@ public class EscalonadorGUI extends Application {
 	private static VBox criaHistoricoColorido() {
 
 		HBox HLinha = new HBox();
-		
+
 		// criaRegua();
 		areaHistorico.getChildren().add(criaRegua());
 
@@ -155,12 +152,11 @@ public class EscalonadorGUI extends Application {
 			rect = new Rectangle(20, 25);
 			HBox container = new HBox();
 			container.getChildren().add(rect);
-			
-			
+
 			// cima, direita, baixo, esquerda
 			HLinha.setPadding(new Insets(2, 2, 0, 10));
 			container.setPadding(new Insets(0, 2, 0, 0));
-			
+
 			if (linhas[i] == StatusProcesso.NAOEXISTE.toString().charAt(0)) {// inexistente
 				rect.setFill(Color.web(COLOR_NAO_EXISTE));
 				HLinha.getChildren().add(container);
@@ -187,8 +183,39 @@ public class EscalonadorGUI extends Application {
 		return areaHistorico;
 	}
 
-	
-	private static VBox criaLegendaDeCores(){
+	private static HBox criaAreaDeConfiguracao() {
+		// configs
+		HBox areaConfig = new HBox();
+
+		VBox colunaQuantum = new VBox();
+		Label lbQuantum = new Label("Quantum");
+		TextField tfQuantum = new TextField();
+
+		// adicionando o label e o textfield posteriormente
+		colunaQuantum.getChildren().add(lbQuantum);
+		colunaQuantum.getChildren().add(tfQuantum);
+
+		VBox colunaEscalonador = new VBox();
+
+		// top, right, bottom, left
+		colunaEscalonador.setPadding(new Insets(0, 0, 10, 20));
+
+		Label lbEscalonadores = new Label("Tipos de Escalonadores");
+		final ComboBox<String> escalonadoresComboBox = new ComboBox<String>();
+		escalonadoresComboBox.getItems().addAll("Roudi Robin");
+
+		// label e combobox
+		colunaEscalonador.getChildren().add(lbEscalonadores);
+		colunaEscalonador.getChildren().add(escalonadoresComboBox);
+
+		areaConfig.getChildren().add(colunaQuantum);
+		areaConfig.getChildren().add(colunaEscalonador);
+		
+		return areaConfig;
+		
+	}
+
+	private static VBox criaLegendaDeCores() {
 		VBox container = new VBox();
 		// cima, direita, baixo, esquerda
 		container.setPadding(new Insets(10, 0, 0, 0));
@@ -196,40 +223,37 @@ public class EscalonadorGUI extends Application {
 //                "-fx-border-insets: 5;\n" +
 //                "-fx-border-width: 3;\n" +
 //                "-fx-border-style: dashed;\n";
-		
-		
 
 		container.getChildren().add(new Separator());
-		//container.getChildren().add(new Label("Legenda"));
-		
+		// container.getChildren().add(new Label("Legenda"));
+
 		GridPane grid = new GridPane();
 		grid.setVgap(5);
 		grid.setHgap(2);
-		
-		Label legenda = new Label("*Legenda*"); 
+
+		Label legenda = new Label("*Legenda*");
 		legenda.setPadding(new Insets(10, 0, 5, 0));
 		grid.add(legenda, 2, 0);
-		
+
 		// column, row
 		// Inexistente
-		grid.add(new Rectangle(30,30, Color.web(COLOR_NAO_EXISTE)), 0, 1);
+		grid.add(new Rectangle(30, 30, Color.web(COLOR_NAO_EXISTE)), 0, 1);
 		grid.add(new Label("Inexistente"), 1, 1);
 		// Executando
-		grid.add(new Rectangle(30,30, Color.web(COLOR_EXECUTANTO)), 0, 2);
+		grid.add(new Rectangle(30, 30, Color.web(COLOR_EXECUTANTO)), 0, 2);
 		grid.add(new Label("Executando"), 1, 2);
 		// Esperando
-		grid.add(new Rectangle(30,30, Color.web(COLOR_ESPERANDO)), 3, 1);
+		grid.add(new Rectangle(30, 30, Color.web(COLOR_ESPERANDO)), 3, 1);
 		grid.add(new Label("Esperando"), 4, 1);
 		// Finalizado
-		grid.add(new Rectangle(30,30, Color.web(COLOR_FINALIZADO)), 3, 2);
+		grid.add(new Rectangle(30, 30, Color.web(COLOR_FINALIZADO)), 3, 2);
 		grid.add(new Label("Finalizado"), 4, 2);
-		
-		
-		//grid.setStyle(cssLayout);
+
+		// grid.setStyle(cssLayout);
 		container.getChildren().add(grid);
 		return container;
 	}
-	
+
 	/**
 	 * Metodo obrigatorio a ser chamado
 	 */
@@ -244,21 +268,21 @@ public class EscalonadorGUI extends Application {
 		VBox historico = criaHistoricoColorido();
 		VBox processos = criaColunaDeProcessos();
 		VBox legendaDeCores = criaLegendaDeCores();
+		HBox areaConfig = criaAreaDeConfiguracao();
 
 		// borderPane.setCenter(criaHistoricoColorido());
 
-		
 		VBox hTitle = new VBox();
 		hTitle.getChildren().add(new Label("De Hero"));
 		hTitle.setStyle("-fx-background-color: #336699;");
-		
+
 		// configuracaoes do border
 		borderPane.setPadding(new Insets(20, 20, 20, 20));
 		borderPane.setLeft(processos);
 		borderPane.setCenter(historico);
 		borderPane.setBottom(legendaDeCores);
-		
-		
+		borderPane.setTop(areaConfig);
+
 		Scene myScene = new Scene(borderPane, WIDTH_WINDOWS, HEIGHT_WINDOWS);
 		primaryStage.setScene(myScene);
 		primaryStage.show();
